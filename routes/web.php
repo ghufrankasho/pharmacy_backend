21853694f0cb69a2a921f9dd6ac199032e58569f
Ghufran Kasho <ghufrankasho2@gmail.com>
2024-12-29 16:11:23 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return file_get_contents(public_path('frontend/index.html'));
 });
